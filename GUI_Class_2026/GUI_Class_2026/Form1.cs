2e4498f2dca0d2e334aa795be7c8682a004cacbe
XTKANEz <jimmy94722@gmail.com>
2026-03-17 21:05:28 +0800

    20260317

--- GUI_Class_2026/GUI_Class_2026/Form1.cs
+++ GUI_Class_2026/GUI_Class_2026/Form1.cs
@@ -16,5 +16,10 @@ namespace GUI_Class_2026
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
