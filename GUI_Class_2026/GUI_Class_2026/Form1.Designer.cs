f09d81820b28ceee265ae67b089bdb0b773117d1
XTKANEz <jimmy94722@gmail.com>
2026-03-24 21:01:58 +0800

    20260324

--- GUI_Class_2026/GUI_Class_2026/Form1.Designer.cs
+++ GUI_Class_2026/GUI_Class_2026/Form1.Designer.cs
@@ -29,23 +29,36 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.btn_ClickMe = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 12);
+            this.label1.Size = new System.Drawing.Size(258, 35);
             this.label1.TabIndex = 0;
             this.label1.Text = "N12310008-蕭丞竣";
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btn_ClickMe
+            // 
+            this.btn_ClickMe.Location = new System.Drawing.Point(14, 56);
+            this.btn_ClickMe.Name = "btn_ClickMe";
+            this.btn_ClickMe.Size = new System.Drawing.Size(256, 73);
+            this.btn_ClickMe.TabIndex = 1;
+            this.btn_ClickMe.Text = "按我一下";
+            this.btn_ClickMe.UseVisualStyleBackColor = true;
+            this.btn_ClickMe.Click += new System.EventHandler(this.btn_ClickMe_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_ClickMe);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +70,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_ClickMe;
     }
 }
 
