bee6b824b4770fd0801e8fac57230be0ec40c7dc
XTKANEz <jimmy94722@gmail.com>
2026-05-12 21:11:38 +0800

    20260512

--- GUI_Class_2026/GUI_Class_2026/FormCalculator.cs
+++ GUI_Class_2026/GUI_Class_2026/FormCalculator.cs
@@ -24,10 +24,26 @@ namespace GUI_Class_2026
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int a = Int32.Parse(textBox1.Text);
-            int b = Int32.Parse(textBox2.Text);
-            int sum = a+b;
-            label3.Text = sum.ToString();
+            try
+            {
+                int[] number = new int[10];
+                number[0] = 1 ;
+                number[1] = 2 ;
+                MessageBox.Show(number[10].ToString());
+                int a = Int32.Parse(textBox1.Text);
+                int b = Int32.Parse(textBox2.Text);
+                int sum = a+b;
+                label3.Text = sum.ToString();
+            }catch(System.FormatException ex)
+            {
+                MessageBox.Show(ex.Message+"輸入的不可以是標點符號，請輸入整數");
+            }catch(IndexOutOfRangeException ex) 
+            {
+                MessageBox.Show(ex.Message+"宣告陣列爆掉了");
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message+"輸入有問題，但我不知道哪裡錯了");
+            }
         }
     }
 }
